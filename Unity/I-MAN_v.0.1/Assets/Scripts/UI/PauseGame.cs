29a96825281e373a7df443907e7b4292d5146e22
nuuriazf <nuriellez@gmail.com>
2022-03-04 02:35:35 +0100

    Primera implementación del menú de pausa
    Con slow. Revisar botones etc etc

---
+++ Unity/I-MAN_v.0.1/Assets/Scripts/UI/PauseGame.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PauseGame : MonoBehaviour
+{
+
+    public GameObject pauseMenuScreen;
+    /*  public bool isPaused;
+      public PlayerInput playerInput;
+      [SerializeField] private GameObject pauseMenuPanel;
+
+
+      public void TogglePause(InputAction.CallbackContext context)
+      {
+          if (!pauseMenuPanel.activeInHierarchy)
+          {
+              Time.timeScale = 0;
+              playerInput.SwitchCurrentActionMap("UI");
+              pauseMenuPanel.SetActive(true);
+              InputSystem.settings.updateMode = InputSettings.UpdateMode.ProcessEventsInDynamicUpdate;
+              Debug.Log("Game Paused");
+              //isPaused = true;
+          }
+
+          else 
+          {
+              Time.timeScale = 1;
+              playerInput.SwitchCurrentActionMap("Player");
+              pauseMenuPanel.SetActive(false);
+              InputSystem.settings.updateMode = InputSettings.UpdateMode.ProcessEventsInFixedUpdate;
+              Debug.Log("Game Unpaused");
+
+          }
+
+      }
+
+      */
+
+    public void Pause()
+    {
+        Time.timeScale = 0.2f;
+        pauseMenuScreen.SetActive(true);
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        pauseMenuScreen.SetActive(false);
+
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
